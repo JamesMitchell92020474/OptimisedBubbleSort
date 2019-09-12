242059b7ed83b4703621fe053d633a08c3259a73
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-12 14:17:10 +1200

    OptimisedBubbleSort Initial

---
+++ src/optimisedbubblesort/OptimisedBubbleSort.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package optimisedbubblesort;
+
+/**
+ *
+ * @title ObjectBubbleSort.java
+ * @author James Mitchell 92020474
+ * @date 12/09/2019
+ * 
+ */
+
+public class OptimisedBubbleSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
